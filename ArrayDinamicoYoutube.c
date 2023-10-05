0962b4421a8296430e41c1510b8b8e7465aefd09
Tomas947 <tomassperezpoveda@gmail.com>
2023-10-05 04:36:39 -0300

    Codigo del video

---
+++ ArrayDinamicoYoutube.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Escriba un programa que lea un array de caracteres, carácter a carácter e imprima dicho array carácter a carácter.
+La introducción de los datos debe finalizar con el salto de linea */
+
+char *leerMensaje(void){
+
+    char *pChar;
+    char *nuevo_pChar;
+    int ultimo;
+    int nChar;
+    int contador;
+
+
+    ultimo = 0;
+    nChar = 1;
+    contador = 0;
+
+
+    pChar = (char*)malloc(sizeof(char)* nChar);
+
+    if(pChar == NULL){
+        perror("malloc: ");
+        return NULL;
+    }
+
+    scanf("%c", &(*(pChar + ultimo)));
+
+    while(*(pChar + ultimo) != '\n'){
+
+
+        nChar++;
+
+        nuevo_pChar = (char*) realloc(pChar, sizeof(char) * (nChar));//es como malloc pero para aumentar el tamaño no para inicializar
+
+        if(nuevo_pChar == NULL){
+            perror("realloc:");
+            free(pChar);
+            return NULL;
+        }
+
+         ultimo++;
+         pChar = nuevo_pChar;
+         contador++;
+
+         scanf("%c", &(pChar[ultimo]));
+
+    }
+
+    (*(pChar + ultimo)) = '\0';
+
+    printf("La cantidad de elementos que posse el vector es: [%i]\n\n", contador);
+
+    return pChar;
+
+}
+
+
+int main(void){
+
+
+    char *msg;
+    int i;
+
+    printf("Introdusca una oracion: \n");
+    msg = leerMensaje();
+
+    printf("El mensaje es: \n");
+
+    if(msg != NULL){
+
+        for(i = 0; *(msg + i) != '\0'; i++){
+            printf("%c", *(msg + i) );
+        }
+        printf("\n");
+    }
+
+    free(msg);
+
+    return 0;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
